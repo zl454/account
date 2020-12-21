fa0e4d48d44b23bbe5f708eb66f89aed18e5d251
zl454 <531720074@qq.com>
2020-12-21 08:58:02 +0800

    删除无用图片，添加快捷路径

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
+const path = require('path')
 module.exports = {
-  publicPath: ""
+  publicPath: "",
+  configureWebpack: (config) => {
+    config.resolve = { // 配置解析别名
+      extensions: ['.js', '.json', '.vue'],
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        'components': path.resolve(__dirname, './src/components'),
+        // 'common': path.resolve(__dirname, './src/common'),
+        // 'api': path.resolve(__dirname, './src/api'),
+        // 'router': path.resolve(__dirname, './src/router'),
+        // 'views': path.resolve(__dirname, './src/views'),
+        // 'data': path.resolve(__dirname, './src/data'),
+        // 'public': path.resolve(__dirname, 'public')
+      }
+    }
+  },
 };
