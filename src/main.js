2dfcefede4f8daee68cdab66875f2b7fcf6268da
zl454 <531720074@qq.com>
2020-12-21 09:07:27 +0800

    取消按需引入 vant ，采用全局应用的形式

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vant from '../node_modules/vant';
+// import Vant from '../node_modules/vant';
+import Vant from 'vant';
 import 'vant/lib/index.css';
 
 Vue.use(Vant);
