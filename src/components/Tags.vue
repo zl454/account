51f11d7a4b59a1fae10d8ca7f6249932aaa498ce
zl454 <531720074@qq.com>
2020-12-28 22:43:10 +0800

    改造 tag 标签

--- src/components/Tags.vue
+++ src/components/Tags.vue
@@ -1,20 +1,40 @@
 <template>
   <div class="tags-container">
-    <van-tag
+    <van-grid
+      square
+      clickable
+      :border="false"
+    >
+      <van-grid-item
+        v-for="(item, index) in tagsList"
+        :key="index"
+        icon="bag-o"
+        :text='item'
+        :class="{'tags-bg':index===current}"
+        @click="toggleTag(index,$event)"
+      />
+    </van-grid>
+    <!-- <van-tag
       round
       v-for="(item, index) in tagsList"
       :key="index"
       :class="{'tags-bg':index===current}"
       @click="toggleTag(index,$event)"
-    >{{item}}</van-tag>
-
+      size="large"
+    >
+      <van-icon name="chat-o" />
+      {{item}}
+    </van-tag> -->
   </div>
 </template>
 
 <script>
 import Vue from "vue";
-import { tag } from "vant";
+import { tag, Icon, Grid, GridItem } from "vant";
 Vue.use(tag);
+Vue.use(Icon);
+Vue.use(Grid);
+Vue.use(GridItem);
 export default {
   data() {
     return {
@@ -31,17 +51,20 @@ export default {
   methods: {
     toggleTag(index, event) {
       this.current = index;
+      console.log(this.current);
       this.$emit("update-taglist", event.target.textContent);
     },
   },
 };
 </script>
 <style scoped>
-.tags-container > span {
+/* .tags-container > span {
   padding: 5px 15px;
   margin: 5px 5px;
   cursor: pointer;
-}
+  display: inline-flex;
+  flex-direction: column;
+} */
 .tags-bg {
   background-color: #5a5a5a;
 }
